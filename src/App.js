523bf5b45485ae0ccc18562189489272626a13c1
ddo0ii <soyeong5857@gmail.com>
2022-09-23 17:34:17 +0900

    14.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import "./App.css";
 
 function App() {
   return (
-    <HashRouter>
-      <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-    </HashRouter>
+    <>
+      <HashRouter>
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </HashRouter>
+      <footer></footer>
+    </>
   );
 }
 
