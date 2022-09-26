94eda008707c59dac4faf840e1ea4d69bfe100cf
ddo0ii <soyeong5857@gmail.com>
2022-09-26 15:05:39 +0900

    14.4 Redirecting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
+function Detail({ location }) {
+  console.log(location);
   return <span>hello</span>;
 }
 export default Detail;
